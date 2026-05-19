375c795d32316ae3b17795cbb991f251db0ac108
Alexander Michael Skibinski <skibins5@msu.edu>
2026-05-19 15:56:52 -0400

    Connected to sqlite

--- main.cpp
+++ main.cpp
@@ -2,3 +2,27 @@
  * @file main.cpp
  * @author alexs
  */
+
+#include <iostream>
+#include <sqlite3.h>
+
+/// this is a test
+int main() {
+
+    sqlite3* db;
+
+    int result = sqlite3_open("../finance.db", &db);
+
+    if(result == SQLITE_OK)
+    {
+        std::cout << "Database connected successfully\n";
+    }
+    else
+    {
+        std::cout << "Connection failed\n";
+    }
+
+    sqlite3_close(db);
+
+    return 0;
+}
